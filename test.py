d542055d50dcc2fd7046ccb1c0f782db2c415576
fady emil <fadymobiledev@gmail.com>
2025-02-18 15:29:58 +0200

    fast test

---
+++ test.py
@@ -0,0 +1,16 @@
+def myFun():
+    print("fady");
+
+def myFunct2(a,b):
+    print(a,b)
+    return a+b
+
+myFun()
+
+result = myFunct2(5, 10)
+print(result)
+import math
+print(math.sin(math.pi/2));
+
+from math import *
+print(sin(pi/2))
